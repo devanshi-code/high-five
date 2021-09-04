511788fdd44fe7a0613ec6fc07c38f40c6797b80
devanshi mishra <devu2732@gmail.com>
2021-09-04 21:41:14 +0530

    organize

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Upcoming from './components/UpcomingEvents';
 import Header from './components/Header.component';
 import Contact from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.css';
+import Organize from './components/organize';
 // import 'bootstrap/dist/js/bootstrap.js';
 
 const App = () => {
   return( 
     <>
     <Navbar/> 
+    <organize/>
     <Upcoming/>
     <Contact />
 
